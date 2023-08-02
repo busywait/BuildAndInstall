0c53bfacd3faae25d9b9974a1330d70b71c25450
busywait <busywait@users.noreply.github.com>
2023-08-02 11:26:45 +0100

    Reduce downloads and copying; add helpful usage info

--- RmgUnpackedAppImage.sh
+++ RmgUnpackedAppImage.sh
@@ -1,11 +1,20 @@
 #!/usr/bin/env bash
 set -ex
 
-#SJM merging steps from 
-#https://github.com/Rosalie241/RMG/blob/master/.github/workflows/build.yml
-#in to the local build script
+if [ "$1" = "--help" ] ||
+    [ "$1" = "-h" ]
+then
+    echo "$0 [Build Config] [Thread Count]"
+    echo "Builds an unpacked .AppImage folder for RMG using build tools"
+    echo "installed in an Ubuntu container environment. -march=native is"
+    echo "passed to the compiler to allow all features of the CPU to be"
+    echo "for optimisation. "
+    exit
+fi
 
-script_dir="$(dirname "$0")"
+#SJM this script merges steps from RMG/.github/workflows/build.yml with
+#the local build script RMG/Source/Scripts/build.sh
+
 script_dir="$(realpath "$script_dir")"
 toplvl_dir="$(realpath "$script_dir/../RMG/")"
 build_config="${1:-Release}"
@@ -17,25 +26,30 @@ install_dir="/home/deck/Applications/RMG-install"
 container_cmd="distrobox-enter ubuntu -- "
 threads="${2:-$(nproc)}"
 
-if [ "$1" = "--help" ] ||
-    [ "$1" = "-h" ]
-then
-    echo "$0 [Build Config] [Thread Count]"
-    exit
+#SJM Assume that the build environment is complete and up-to-date
+#if RMG/squashfs-root already exists
+if[ -d "$(toplvl_dir)/squashfs-root" ]; then
+    $container_cmd "$(script_dir)/RmgGetOrUpdateBuildTools.sh"
 fi
+#...to force an update remove squash-root before running this script
 
 #SJM git commands assume that we are working inside the repo
-#and so does the linuxdeploy (AppImage) script
+#and so does the linuxdeploy (AppImage) command
 cd "$toplvl_dir"
 $container_cmd git pull
 
-#SJM current cmake on Linux does this for us anyway
+#SJM current cmake on Linux creates missing folders for us, but documentation
+#advises that we should create them
 mkdir -p "$build_dir" "$bin_dir"
 
-#SJM play with native-only compiles allowing GCC to use any extension
+#SJM default to native-only compiles allowing GCC to use any extension
 #supported by the CPU on the build machine
 export CXXFLAGS="-march=native"
 export CFLAGS="-march=native"
+# TODO: check which dependencies also respect these suggestions
+# TODO: move the flags in to a cmake target (eg, "NativeRelease")
+# TODO: get the expanded list of flags and create a SteamDeck target
+# (eg SteamDeckRelease)
 
 #SJM I'm not sure what this is for but the github build action does it
 export GITHUB_ENV="$toplvl_dir/git-ver.txt"
@@ -62,14 +76,16 @@ $container_cmd "$toplvl_dir/Package/AppImage/Create.sh --folder-only"
 #compressed AppImage file
 #$container_cmd "$script_dir/_buildRmgAppImageFolder.sh"
 
-#SJM Optional: run outside of the ubuntu distrobox to remove any of the 
-#bundled libraries that are already on the Steam Deck before they are 
-#copied to the install location
+#SJM Optional: run this command outside of the ubuntu distrobox to remove 
+#any of the bundled libraries that are already on the Steam Deck before 
+#they are copied to the install location
 mkdir "$bin_dir/../lib-not-needed" && for g in $(for f in $(find "$bin_dir/usr/lib" -type f -printf "%f\n"); do ldconfig -p  | grep -o "$f" | uniq; done); do mv "$bin_dir/usr/lib/$g" "$bin_dir/../lib-not-needed"; done
 
-#_SJM copy everything in the AppImage directory
-rm -rf "$install_dir"
-cp -r "$bin_dir" "$install_dir"
+#SJM move the AppImage directory to the install path, overwrite existing
+rm -rf "$(install_dir).old"
+mv "$(install_dir)" "$(install_dir).old"
+mv "$bin_dir" "$install_dir"
 
 set +x
-echo RmgUpackedAppImage Done
+echo "RMG has been built and installed to $(install_dir)"
+echo "Run RMG with \"$(install_dir)/App.wrapped\""
