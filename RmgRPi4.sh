c39c3e326628049ebe570e307fd0720b10819c74
busywait <busywait@users.noreply.github.com>
2023-08-08 10:27:21 +0100

    Rename RmgPi4.sh so that it is easier to type with autocomplete

--- RmgRPi4.sh
+++ RmgRPi4.sh
@@ -21,10 +21,17 @@ toplvl_dir="$(realpath "$script_dir/../RMG/")"
 build_config="${1:-Release}"
 build_arch=ARM
 export src_dir="$toplvl_dir"
-export build_dir="$toplvl_dir/Build/ARM/$build_config"
-export bin_dir="$toplvl_dir/Bin/ARM/$build_config"
+export build_dir="$toplvl_dir/Build/$build_arch/$build_config"
+export bin_dir="$toplvl_dir/Bin/$build_arch/$build_config"
 install_dir="$(realpath ~/RMG-install)"
 threads=2
+
+cmake_strip_args=" --strip "
+# Don't strip debug builds (configurations starting with Deb)
+if [[ $build_config =~ ^Deb+ ]]
+then
+   cmake_strip_args=" "
+fi
 
 #SJM git commands assume that we are working inside the repo
 #and so does the linuxdeploy (AppImage) command
@@ -39,7 +46,7 @@ mkdir -p "$build_dir" "$bin_dir"
 export CXXFLAGS="-march=native"
 export CFLAGS="-march=native"
 # TODO: check which dependencies also respect these suggestions
-# TODO: move the flags in to a cmake target (eg, "NativeRelease")
+# TODO: move the flags in to a cmake configuration (eg, "NativeRelease")
 # TODO: get the expanded list of flags and create a SteamDeck target
 # (eg SteamDeckRelease) for cross-compiles
 
@@ -47,12 +54,15 @@ export CFLAGS="-march=native"
 export GITHUB_ENV="$toplvl_dir/git-ver.txt"
 echo "GIT_REVISION=$(git describe --tags --always)" >> $GITHUB_ENV
 
-cmake --fresh -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="$build_config" -DPORTABLE_INSTALL="ON" -DUPDATER=ON -DAPPIMAGE_UPDATER=ON -DCMAKE_INSTALL_PREFIX="$build_arch" -DRPI4=ON -G "Ninja"
+# The AppImage is PORTABLE_INSTALL=OFF
+# When PORTABLE_INSTALL=ON the CMAKE_INSTALL_PREFIX will be overwritten to an empty string in RMG/CMakeLists.txt
+cmake --fresh -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="$build_config" -DPORTABLE_INSTALL="ON" -DUPDATER=ON -DAPPIMAGE_UPDATER=ON -DRPI4=ON -G "Ninja"
 #The local build command line
 #cmake -S "$toplvl_dir" -B "$build_dir" -DCMAKE_BUILD_TYPE="$build_config" -DPORTABLE_INSTALL=ON -G "Ninja"
 
 cmake --build "$build_dir" --parallel "$threads" -v
-cmake --install "$bin_dir" --prefix=""
+# a --prefix given for --install here will be prepended to any CMAKE_INSTALL_PREFIX given above
+cmake --install "$build_dir" $cmake_strip_args --prefix="$bin_dir"
 
 #if [[ $(uname -s) = *MINGW64* ]]
 #then
@@ -61,7 +71,10 @@ cmake --install "$bin_dir" --prefix=""
 
 #SJM move the AppImage directory to the install path, overwrite existing
 rm -rf "$install_dir.old"
-mv "$install_dir" "$install_dir.old"
+if [ -d "$install_dir" ]
+then
+    mv "$install_dir" "$install_dir.old"
+fi
 mv "$bin_dir" "$install_dir"
 
 set +x
